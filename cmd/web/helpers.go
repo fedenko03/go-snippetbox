47140c5a7dda4f19a0daee2da3de1911de5e2eab
alexey F <52556121+fedenko03@users.noreply.github.com>
2022-11-07 16:25:57 +0600

    Chapter 11.6 11.7 User authorization CSRF protection

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/go-playground/form/v4"
+	"github.com/justinas/nosurf"
 	"net/http"
 	"runtime/debug"
 	"time"
@@ -12,8 +13,10 @@ import (
 
 func (app *application) newTemplateData(r *http.Request) *templateData {
 	return &templateData{
-		CurrentYear: time.Now().Year(),
-		Flash:       app.sessionManager.PopString(r.Context(), "flash"),
+		CurrentYear:     time.Now().Year(),
+		Flash:           app.sessionManager.PopString(r.Context(), "flash"),
+		IsAuthenticated: app.isAuthenticated(r),
+		CSRFToken:       nosurf.Token(r),
 	}
 }
 
@@ -49,26 +52,21 @@ func (app *application) render(w http.ResponseWriter, status int, page string, d
 }
 
 func (app *application) decodePostForm(r *http.Request, dst any) error {
-	// Call ParseForm() on the request, in the same way that we did in our
-	// createSnippetPost handler.
 	err := r.ParseForm()
 	if err != nil {
 		return err
 	}
-	// Call Decode() on our decoder instance, passing the target destination as
-	// the first parameter.
 	err = app.formDecoder.Decode(dst, r.PostForm)
 	if err != nil {
-		// If we try to use an invalid target destination, the Decode() method
-		// will return an error with the type *form.InvalidDecoderError.We use
-		// errors.As() to check for this and raise a panic rather than returning
-		// the error.
 		var invalidDecoderError *form.InvalidDecoderError
 		if errors.As(err, &invalidDecoderError) {
 			panic(err)
 		}
-		// For all other errors, we return them as normal.
 		return err
 	}
 	return nil
+}
+
+func (app *application) isAuthenticated(r *http.Request) bool {
+	return app.sessionManager.Exists(r.Context(), "authenticatedUserID")
 }
